c0f2fa5a4b9b3e4f192d7832c088db74f530311b
Xu yang <nimaster1989@gmail.com>
2020-06-23 03:04:31 -0500

    Initial commit

---
+++ app/src/main/java/comp3350/Group2/areyouhungry/ui/favorites/FavoritesFragment.java
@@ -0,0 +1,33 @@
+package comp3350.Group2.areyouhungry.ui.favorites;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import comp3350.Group2.areyouhungry.R;
+
+public class FavoritesFragment extends Fragment {
+
+    private FavoritesViewModel favoritesViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        favoritesViewModel =
+                ViewModelProviders.of(this).get(FavoritesViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_favorites, container, false);
+        final TextView textView = root.findViewById(R.id.text_favorites);
+        favoritesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/comp3350/Group2/areyouhungry/ui/more/MoreViewModel.java
@@ -0,0 +1,19 @@
+package comp3350.Group2.areyouhungry.ui.more;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MoreViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MoreViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is more fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
