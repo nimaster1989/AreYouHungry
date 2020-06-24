c5072492f096a179ff03653cc88be0831656c2ce
Xu yang <nimaster1989@gmail.com>
2020-06-24 16:30:48 -0500

    Database in construction 10%

--- app/src/main/java/comp3350/Group2/areyouhungry/Services.java
+++ app/src/main/java/comp3350/Group2/areyouhungry/Services.java
@@ -1,4 +1,36 @@
 package comp3350.Group2.areyouhungry;
 
+import comp3350.Group2.areyouhungry.presistence.DataAccessStub;
+
 public class Services {
+    private static DataAccessStub dataAccessService = null;
+
+    public static DataAccessStub createDataAccess(String dbName)
+    {
+        if (dataAccessService == null)
+        {
+            dataAccessService = new DataAccessStub(dbName);
+            //dataAccessService.open(Main.dbName);
+        }
+        return dataAccessService;
+    }
+
+    public static DataAccessStub getDataAccess(String dbName)
+    {
+        if (dataAccessService == null)
+        {
+            System.out.println("Connection to data access has not been established.");
+            System.exit(1);
+        }
+        return dataAccessService;
+    }
+
+    public static void closeDataAccess()
+    {
+        if (dataAccessService != null)
+        {
+            //dataAccessService.close();
+        }
+        dataAccessService = null;
+    }
 }
--- app/src/main/java/comp3350/Group2/areyouhungry/presistence/DataAccessStub.java
+++ app/src/main/java/comp3350/Group2/areyouhungry/presistence/DataAccessStub.java
@@ -1,6 +1,24 @@
 package comp3350.Group2.areyouhungry.presistence;
 
-import comp3350.*;
-public class DataAccessStub {
+import java.util.ArrayList;
 
+import comp3350.*;
+import comp3350.Group2.areyouhungry.MainActivity;
+import comp3350.Group2.areyouhungry.objects.Foods;
+
+public class DataAccessStub {
+    private String dbName;
+    private String dbType = "stub";
+
+    private ArrayList<Foods> foods;
+
+    public DataAccessStub(String dbName)
+    {
+        this.dbName = dbName;
+    }
+
+    public DataAccessStub()
+    {
+        //this(MainActivity.dbName);
+    }
 }
