3a2cedfa6442b810328736c003dbead5eb0dec21
Xu yang <nimaster1989@gmail.com>
2020-06-26 23:10:58 -0500

    favorite pages in construction 10%
    favorite pages in construction 10%

---
+++ app/src/main/java/comp3350/Group2/areyouhungry/FoodDetailFragment.java
@@ -0,0 +1,72 @@
+package comp3350.Group2.areyouhungry;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import comp3350.Group2.areyouhungry.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Food detail screen.
+ * This fragment is either contained in a {@link FoodListActivity}
+ * in two-pane mode (on tablets) or a {@link FoodDetailActivity}
+ * on handsets.
+ */
+public class FoodDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public FoodDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.food_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.food_detail)).setText(mItem.details);
+        }
+
+        return rootView;
+    }
+}
