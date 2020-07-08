45381f79f5f2de194c9b9ada878e3919be52a66b
ColeVoelpel <66435041+ColeVoelpel@users.noreply.github.com>
2020-07-08 15:56:06 -0500

    Add ability to go to food detail page from a preferred search recipe

--- app/src/main/java/comp3350/Group2/areyouhungry/ui/preferred/PreferredSearchActivity.java
+++ app/src/main/java/comp3350/Group2/areyouhungry/ui/preferred/PreferredSearchActivity.java
@@ -2,9 +2,12 @@ package comp3350.Group2.areyouhungry.ui.preferred;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,6 +17,8 @@ import java.util.ArrayList;
 import comp3350.Group2.areyouhungry.R;
 import comp3350.Group2.areyouhungry.business.AccessFoods;
 import comp3350.Group2.areyouhungry.objects.Food;
+import comp3350.Group2.areyouhungry.ui.all_food.FoodDetailActivity;
+import comp3350.Group2.areyouhungry.ui.all_food.FoodDetailFragment;
 
 
 public class PreferredSearchActivity extends AppCompatActivity {
@@ -51,6 +56,19 @@ public class PreferredSearchActivity extends AppCompatActivity {
             };
             System.out.println("list view");
             final ListView listView = (ListView) findViewById(R.id.preferred_pick);
+            //Creating an onClickListener for clicking on the food selected
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    //Gets the food selected from the list
+                    Food food = (Food) foodList.get(position);
+                    Context context = view.getContext();
+                    //Change the content of the application
+                    Intent intent = new Intent(context, FoodDetailActivity.class);
+                    intent.putExtra(FoodDetailFragment.ARG_ITEM_ID, food.getFoodID());
+                    context.startActivity(intent);
+                }
+            });
             listView.setAdapter(foodArrayAdapter);
 
         }
