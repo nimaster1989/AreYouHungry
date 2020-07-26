73fbb31f6b5e11793863ea95ed4e48c9208a6d04
Xu yang <nimaster1989@gmail.com>
2020-07-26 18:22:52 -0500

    set up new java classes for(Categorys & Food-Categorys) matching the HSQLDB database

---
+++ app/src/main/java/comp3350/Group2/areyouhungry/objects/Categorys.java
@@ -0,0 +1,18 @@
+package comp3350.Group2.areyouhungry.objects;
+
+public class Categorys {
+    private int categoryID;
+    private String categoryName;
+    public Categorys(int categoryID,String categoryName){
+        this.categoryID = categoryID;
+        this.categoryName  = categoryName;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+}
