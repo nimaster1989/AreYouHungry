608a79b5e424a89403cd4fa3839014b2ef0aa67f
Xu yang <nimaster1989@gmail.com>
2020-07-26 22:24:50 -0500

    change the test structure again

--- app/src/test/java/comp3350/Group2/areyouhungry/tests/AllTests.java
+++ app/src/test/java/comp3350/Group2/areyouhungry/tests/AllTests.java
@@ -3,9 +3,11 @@ package comp3350.Group2.areyouhungry.tests;
 import org.junit.Test;
 import junit.framework.TestSuite;
 
+import comp3350.Group2.areyouhungry.persistence.DataAccess;
 import comp3350.Group2.areyouhungry.tests.business.AccessFoodTest;
 import comp3350.Group2.areyouhungry.tests.business.PreferredSearchTest;
 import comp3350.Group2.areyouhungry.tests.objects.FoodTest;
+import comp3350.Group2.areyouhungry.tests.persistence.DataAccessTest;
 
 //This class when run tests all the other testing suites
 public class AllTests {
@@ -15,25 +17,26 @@ public class AllTests {
     //This method runs ALL the test suites
     public static TestSuite suite(){
         suite = new TestSuite("All Tests");
-        testDatabase();
-        testPreferredSearch();
-        testFood();
+        testBusiness();
+        testObject();
+        testPersistence();
         return suite;
     }
 
     //This will add all the tests from the Database testing suite
+
     @Test
-    private static void testDatabase(){
+    private static void testBusiness(){
+        suite.addTestSuite(PreferredSearchTest.class);
         suite.addTestSuite(AccessFoodTest.class);
     }
 
     @Test
-    private static void testPreferredSearch(){
-        suite.addTestSuite(PreferredSearchTest.class);
+    private static void testObject(){
+        suite.addTestSuite(FoodTest.class);
     }
 
-    @Test
-    private static void testFood(){
-        suite.addTestSuite(FoodTest.class);
+    private static void testPersistence(){
+        suite.addTestSuite(DataAccessTest.class);
     }
 }
