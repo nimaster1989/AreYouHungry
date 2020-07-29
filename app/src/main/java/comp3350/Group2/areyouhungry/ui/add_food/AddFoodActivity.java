b8dbb5cbc34ce9c2db7f809a2d112ca6535b71ed
Xu Yang <56135603+nimaster1989@users.noreply.github.com>
2020-07-29 16:34:14 -0500

    add food page update

--- app/src/main/java/comp3350/Group2/areyouhungry/ui/add_food/AddFoodActivity.java
+++ app/src/main/java/comp3350/Group2/areyouhungry/ui/add_food/AddFoodActivity.java
@@ -51,14 +51,73 @@ public class AddFoodActivity extends AppCompatActivity {
 
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-            String getting = sharedPreferences.getString(s, "");
-            System.out.println(s+"set as:"+getting);
-            switch (getting){
-                case "ingredient":
-                    EditTextPreference signaturePreference = findPreference("ingredient2");
-                    if (signaturePreference != null) {
-                        signaturePreference.setVisible(true);
-                    }
+            if(!s.equals("favourite")){
+                String getting = sharedPreferences.getString(s, "");
+                System.out.println(s+"set as:"+getting);
+                switch (s) {
+                    case "ingredient":
+                        EditTextPreference ep2 = findPreference("ingredient2");
+                        if (ep2 != null && !getting.equals("")) {
+                            ep2.setVisible(true);
+                            break;
+                        }
+                    case "ingredient2":
+                        ep2 = findPreference("ingredient2");
+                        ep2.setSummary(getting);
+                        EditTextPreference ep3 = findPreference("ingredient3");
+                        if (ep3 != null && !getting.equals("")) {
+                            ep3.setVisible(true);
+                            break;
+                        }
+                    case "ingredient3":
+                        ep3 = findPreference("ingredient3");
+                        ep3.setSummary(getting);
+                        EditTextPreference ep4 = findPreference("ingredient4");
+                        if (ep4 != null && !getting.equals("")) {
+                            ep4.setVisible(true);
+                            break;
+                        }
+                    case "ingredient4":
+                        ep4 = findPreference("ingredient4");
+                        ep4.setSummary(getting);
+                        EditTextPreference ep5 = findPreference("ingredient5");
+                        if (ep5 != null && !getting.equals("")) {
+                            ep5.setVisible(true);
+                            break;
+                        }
+                    case "instruction":
+                        EditTextPreference inp2 = findPreference("instruction2");
+                        if (inp2 != null && !getting.equals("")) {
+                            inp2.setVisible(true);
+                            break;
+                        }
+                    case "instruction2":
+                        inp2 = findPreference("instruction2");
+                        inp2.setSummary(getting);
+                        EditTextPreference inp3 = findPreference("instruction3");
+                        if (inp3 != null && !getting.equals("")) {
+                            inp3.setVisible(true);
+                            break;
+                        }
+                    case "instruction3":
+                        inp3 = findPreference("instruction3");
+                        inp3.setSummary(getting);
+                        EditTextPreference inp4 = findPreference("instruction4");
+                        if (inp4 != null && !getting.equals("")) {
+                            inp4.setVisible(true);
+                            break;
+                        }
+                    case "instruction4":
+                        inp4 = findPreference("instruction4");
+                        inp4.setSummary(getting);
+                        EditTextPreference inp5 = findPreference("instruction5");
+                        if (inp5 != null && !getting.equals("")) {
+                            inp5.setVisible(true);
+                            break;
+                        }
+                    default:
+                        break;
+                }
             }
         }
     }
